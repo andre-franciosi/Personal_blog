0a6aabd27e440c42f007e2033f65a8829a873c05
André Franciosi <andref2210@hotmail.com>
2023-02-08 11:57:33 -0300

    Creating a login page

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
+from flask_wtf.csrf import CSRFProtect, CSRFError
+import os
 
 app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+csrf = CSRFProtect(app)
+
+#Create Form class
+
+class NameForm(FlaskForm):
+    name = StringField("User", validators = [DataRequired()]) #Stringfield is the textbox  #With DataRequired the user have to put something in the field
+    password = PasswordField("Password ", validators = [DataRequired()])
+    submit = SubmitField("Log in") #Button
 
 #Creating the first route
 
@@ -17,6 +33,24 @@ def index():
 
 def user(name):
     return render_template('user.html', user_name = name)
+
+#Register page route
+
+@app.route('/register', methods = ['GET', 'POST'])
+
+def register():
+    name = None
+    password = None
+    form = NameForm()
+    #Validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        password = form.password.data
+        form.name.data = ''
+    return render_template('user_register.html',
+    name = name,
+    password = password,
+    form = form)
 
 #error pages
 
