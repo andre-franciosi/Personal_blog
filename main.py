6e93a3842301a7d27015bfadcba462b3386d0213
André Franciosi <andref2210@hotmail.com>
2023-02-10 00:00:14 -0300

    Changing login page

--- main.py
+++ main.py
@@ -1,58 +1,47 @@
-from flask import Flask, render_template, flash
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
-from flask_wtf.csrf import CSRFProtect, CSRFError
+from flask import Flask, render_template, request, session, redirect, url_for
 import os
+
+class User:
+    def __init__(self, id, username, password):
+        self.id =id
+        self.username = username
+        self.password = password
+    
+    def __repr__(self) :
+        return f'<Users: {self.username}>'
+
+users = []
+users.append(User(1,username='Andre',password='123'))
+users.append(User(2,username='Ricardo',password='321'))
 
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-csrf = CSRFProtect(app)
-
-#Create Form class
-
-class NameForm(FlaskForm):
-    name = StringField("User", validators = [DataRequired()]) #Stringfield is the textbox  #With DataRequired the user have to put something in the field
-    password = PasswordField("Password ", validators = [DataRequired()])
-    submit = SubmitField("Log in") #Button
-
 #Creating the first route
 
 @app.route('/')
-
 def index():
     num = [1,2,3,4,5]
     return render_template('index.html',
     num=num)
 
-#User route (localhost:5000/user/name)
-
-@app.route('/user/<name>')
-
-def user(name):
-    return render_template('user.html', user_name = name)
-
 #Register page route
 
-@app.route('/register', methods = ['GET', 'POST'])
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+        username = request.form['username']
+        password = request.form['password']
 
-def register():
-    name = None
-    password = None
-    form = NameForm()
-    #Validate form
-    if form.validate_on_submit():
-        name = form.name.data
-        password = form.password.data
-        form.name.data = ''
-        flash('Everything went alright!')
-    return render_template('user_register.html',
-    name = name,
-    password = password,
-    form = form)
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user.id'] = user.id
+            return redirect(url_for('index'))
+        return redirect(url_for('login'))
 
+    return render_template('login.html')
 #error pages
 
 # Invalida URL (404)
